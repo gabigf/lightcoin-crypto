a8fb7896662c75ad79dfc37be5b71872a260bc79
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-06 21:35:08 +0000

    Added solution to LHL coin exercise

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 
-  constructor(amount) {
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.value;
   }
+}
 
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+  
+}
+
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
 }
 
 
@@ -18,12 +37,20 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const myAccount = new Account('a-nu-start');
 
-t2 = new Withdrawal(9.99);
+console.log(`Starting balance: `, myAccount.balance)
+
+t1 = new Withdrawal(50.25, myAccount);
+t1.commit()
+
+
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+
+
+console.log('Ending balance:', myAccount.balance);
